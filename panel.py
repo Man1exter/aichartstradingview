bdec0a693f483b8371657d05f302d47c79cccbf7
Mariusz Perzynski <m.perzyk@interia.pl>
2025-01-08 18:31:54 +0100

    `Refactored panel.py to remove QVBoxLayout and central widget setup, updated TradingView widget styles and dimensions, and added window centering functionality.`

--- panel.py
+++ panel.py
@@ -1,13 +1,13 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
+from PySide6.QtWidgets import QApplication, QMainWindow
 from PySide6.QtWebEngineWidgets import QWebEngineView
+from PySide6.QtCore import Qt
 
 class CryptoChart(QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("Crypto Chart Viewer - TradingView")
-        self.setGeometry(100, 100, 1200, 800)
 
         # Widget WebEngineView do wyświetlenia widgetu TradingView
         self.web_view = QWebEngineView()
@@ -18,20 +18,35 @@ class CryptoChart(QMainWindow):
         <html>
         <head>
             <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
+            <style>
+                body {
+                    margin: 0;
+                    background-color: black;
+                    height: 100%;
+                    overflow: hidden;
+                }
+                #tradingview_widget {
+                    position: absolute;
+                    top: 0;
+                    left: 0;
+                    width: 100%;
+                    height: 100%;
+                }
+            </style>
         </head>
         <body>
             <div id="tradingview_widget"></div>
             <script type="text/javascript">
                 new TradingView.widget({
-                    "width": 1100,
-                    "height": 700,
+                    "width": "100%",
+                    "height": "100%",
                     "symbol": "BINANCE:BTCUSDT",
                     "interval": "1",
                     "timezone": "Etc/UTC",
                     "theme": "dark",
                     "style": "1",
                     "locale": "en",
-                    "toolbar_bg": "#f1f3f6",
+                    "toolbar_bg": "#000000",
                     "enable_publishing": false,
                     "hide_top_toolbar": true,
                     "save_image": false,
@@ -44,13 +59,18 @@ class CryptoChart(QMainWindow):
 
         # Załaduj HTML do WebEngineView
         self.web_view.setHtml(tradingview_html)
+        self.setCentralWidget(self.web_view)
 
-        # Ustawienia układu
-        layout = QVBoxLayout()
-        layout.addWidget(self.web_view)
-        container = QWidget()
-        container.setLayout(layout)
-        self.setCentralWidget(container)
+        # Ustawienia okna
+        self.resize(1200, 800)
+        self.setWindowFlags(Qt.Window | Qt.WindowTitleHint | Qt.WindowCloseButtonHint)
+        self.center_window()
+
+    def center_window(self):
+        screen_geometry = QApplication.primaryScreen().geometry()
+        window_geometry = self.frameGeometry()
+        window_geometry.moveCenter(screen_geometry.center())
+        self.move(window_geometry.topLeft())
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
